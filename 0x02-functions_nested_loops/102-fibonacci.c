75daa1c1f7c7282d782e1737c6462ba81c9ee337
Abdulrashid Abdulrazaq <abdulrazaqabdulrashid@gmail.com>
2022-07-06 16:14:43 -0700

    Task 14.Fibonnaci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,19 +6,18 @@
 int main(void)
 {
 unsigned long count, i, j, k;
-
-	i = 0;
-	j = 1;
+i = 0;
+j = 1;
 
 for (count = 0; count < 50; count++)
 {
 k = i + j;
-											i = j;
-	j = k;
+i = j;
+j = k;
 printf("%lu", k);
-										if (count == 49)
+if (count == 49)
 putchar('\n');
-										else
+else
 {
 printf(", ");
 }
