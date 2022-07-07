b117592bdb601e48362faf4330d595fa6adf91f3
Abdulrashid Abdulrazaq <abdulrazaqabdulrashid@gmail.com>
2022-07-07 09:16:21 -0700

    Task 0.isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper.c - function that checks for uppercase character
+ *
+ * @c: integer varriable to be check
+ * Return: 1 if it is uppercase and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 'a' && c <= 'z');
+
+return (1);
+else
+return (0)
+}
