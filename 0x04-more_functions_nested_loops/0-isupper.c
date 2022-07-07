ad280dd89707497748a58703015d34ab34d27345
Abdulrashid Abdulrazaq <abdulrazaqabdulrashid@gmail.com>
2022-07-07 15:10:37 -0700

    Task 0.isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,9 @@
  * @c: is the int used for the argument of the function.
  * Return: 1 if uppercase character 0 if not
  */
-
 int _isupper(int c)
-
 {
-if (c >= 'A' && c <= 'Z');
-
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
