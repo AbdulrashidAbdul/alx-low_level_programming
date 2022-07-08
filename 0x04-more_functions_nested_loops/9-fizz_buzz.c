a291bcb950af721b706a8b2bf1f696a864b1761c
Abdulrashid Abdulrazaq <abdulrazaqabdulrashid@gmail.com>
2022-07-08 14:56:27 -0700

    Task 9.fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,14 +7,13 @@
 int main(void)
 {
 int i = 1;
-
 while (i <= 100)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
-										else if (i % 3 == 0)
+else if (i % 3 == 0)
 {
 printf("Fizz");
 }
